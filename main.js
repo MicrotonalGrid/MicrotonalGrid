c8c63b3acb6efe59f68ae71e481667c3bae0c743
GridAdmin <gridadmin@inazma.org>
2019-01-20 20:18:31 -0300

    adding translation.

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ osc.start();
 
 osc.connect(audioCtx.destination);
 
+
+let square1 = document.createElement("div");
+
+square1.id = "lol'";
+square1.className = "button2";
+square1.style.cssText="transform:translate(10px, 10px)";
+
+ document.getElementById("matrix").appendChild(square1);
+
+// var Somethign = "<span class=\"button2\"></span>";
+//		document.getElementById("matrix").innerHTML += Somethign;
+
+// var Somethign = "<span class=\"greenSquare\"></span>";
+//		document.getElementById("caissa").innerHTML += Somethign;
+
+
