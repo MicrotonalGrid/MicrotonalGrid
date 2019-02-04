3b225a538f21603d87863ffba2b1821c6a33054d
MicrotonalGrid <46867892+MicrotonalGrid@users.noreply.github.com>
2019-02-04 19:21:34 -0300

    Fixed grid, starting work on polyphonic synth

---
+++ grid.js
@@ -0,0 +1,53 @@
+
+function onMouseClick(event)
+{
+  if(this.className == 'matrixButtOff')
+  {
+    this.className = 'matrixButtOn';
+  }
+  else
+  {
+    this.className = 'matrixButtOff';
+  }
+  console.log(this);
+}
+
+
+function onMouseOver(event)
+{
+  if(event.buttons>0)
+  {
+    if(this.className == 'matrixButtOff')
+    {
+      this.className = 'matrixButtOn';
+    }
+    else
+    {
+      this.className = 'matrixButtOff';
+    }
+    
+  }
+}
+
+function createGrid(buttons)
+{
+  for (var j = 0; j < 16 ; j++)
+  {
+    buttons.push(new Array);
+        
+    for (var i = 0; i < 16 ; i++)
+    {
+          
+      buttons[j].push(document.createElement("div"));
+      buttons[j][i].style.bottom = (i * 37.5) + 5.5;
+      buttons[j][i].style.right = (700- (j * 37.5) + 7.5);
+    
+      buttons[j][i].id = "button"+i+j;
+      buttons[j][i].className = "matrixButtOff";
+      buttons[j][i].addEventListener('mouseover',onMouseOver, false);
+      buttons[j][i].addEventListener('mousedown',onMouseClick, false);
+      document.getElementById("matrix").appendChild(buttons[j][i]);          
+    }
+  }
+}
+
--- main.js
+++ main.js
@@ -1,63 +1,48 @@
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioCtx = new AudioContext();
-var osc = audioCtx.createOscillator();
-//osc.start();
 
-osc.connect(audioCtx.destination);
+let onOff = document.createElement("div");
+onOff.style.bottom = 0;
+onOff.style.right =0;
+onOff.className = "matrixButtOff";
+onOff.addEventListener('click',unmute, false);
+document.getElementById("matrix").appendChild(onOff);   
+let virgin = true;       
+
+
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let audioCtx = new AudioContext();
+let oscArray = new Array;
+
+for(var i = 0; i<16 ; i++)
+{
+  oscArray.push(audioCtx.createOscillator());
+  //let osc = audioCtx.createOscillator();
+}
 
 let buttons = new Array;
+createGrid(buttons);
 
-for (var j = 0; j < 16 ; j++)
+buttons[4][5].className = 'matrixButtOn';
+
+function unmute(event)
 {
-	buttons.push(new Array);
-				
-	for (var i = 0; i < 16 ; i++)
-	{
-					
-        buttons[j].push(document.createElement("div"));
-        
-        let styleText1 = "transform:translate(" + i*100 +"px, " + j*10 + "px)";
-       let styleText12 = "top:" + j*10 + "px; left:" + i*10 + "px";
-        ////console.log(styleText1);
-       // console.log("transform:translate(" + 10 + "px, " + 10 +"px)");
-
-        ///buttons[j][i].cssText=styleText12; //+ (j * 37.5) + 50 + "px, " + (561- (i * 37.5) + 5) + "px)"; //x coord
-       // buttons[j][i].cssText += (561- (i * 37.5) + 5) + "px)";
-        //buttons[j][i].cssText= styleText12;
-        buttons[j][i].style.top = (i * 20) + 5;
-        buttons[j][i].style.left = (j * 20) + 5;
-		buttons[j][i].id = "button"+i+j;
-		buttons[j][i].className = "matrixButton";
-		document.getElementById("matrix").appendChild(buttons[j][i]);						
-	}
+  if(virgin ===true)
+  {
+    osc.start();
+    virgin = false;
+  }
+  if(this.className == 'matrixButtOff')
+  {
+    this.className = 'matrixButtOn';
+    osc.connect(audioCtx.destination);
+    
+  }
+  else
+  {
+    this.className = 'matrixButtOff';
+    
+    osc.disconnect(audioCtx.destination);
+  }
 }
-/*
 
 
-let square1 = document.createElement("div");
 
-square1.id = "lol'";
-square1.className = "matrixButton";
-square1.style.cssText="transform:translate(" + 10 + "px, " + 10 +"px)";
-
- document.getElementById("matrix").appendChild(square1);*/
-
-
-/*
- reference grid loop
-	for (var j:int = 0; j < 16 ; j++)
-			{
-				buttons.push(new Vector.<soundButton>);
-				
-				for (var i:int = 0; i < 16 ; i++)
-				{
-					
-					buttons[j].push(new soundButton());
-					buttons[j][i].y = (561- (i * 37.5) + 5);
-					buttons[j][i].x  = (j * 37.5) + 50;
-					buttons[j][i].visible = true;
-					buttons[j][i].alpha = 0.5
-					this.addChild(buttons[j][i]);						
-                }
-            }
-				*/
