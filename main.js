c4e68db90458f4bf7fa178b90cabd5a98aa98f38
GridAdmin <gridadmin@inazuma.org>
2019-02-05 21:42:55 -0300

    added in chord button that toggles a chord being played on the bottom row.
    TODO :  change it so that it's on the side

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ document.getElementById("matrix").appendChild(onOff);
 let initialised = true;       
 
 
+let playChord = document.createElement("div");
+playChord.style.bottom = 400;
+playChord.style.right =0;
+playChord.className = "matrixButtOff";
+playChord.addEventListener('click',chord, false);
+document.getElementById("matrix").appendChild(playChord);   
+
+
 let AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioCtx = new AudioContext();
 let oscArray = new Array;
@@ -15,7 +23,7 @@ let oscArray = new Array;
 for(var i = 0; i<16 ; i++)
 {
   oscArray.push(audioCtx.createOscillator());
-  let freq = 220 * Math.pow( 2 , (i/12)); //replace 2 and 12 with subdivs and octave size in the future
+  let freq = 440 * Math.pow( 2 , (i/12)); //replace 2 and 12 with subdivs and octave size in the future
   oscArray[i].frequency.setValueAtTime(freq, audioCtx.currentTime);
   console.log(oscArray.length);
   //let osc = audioCtx.createOscillator();
@@ -55,3 +63,32 @@ function unmute(event)
 
 
 
+function chord(event)
+{
+  if(initialised ===true)
+  {
+	  for(var i = 0; i<oscArray.length ; i++)
+	  {
+		  oscArray[i].start();
+		  initialised = false;
+	  }
+  }
+  if(this.className == 'matrixButtOff')
+  {
+	  this.className = 'matrixButtOn';
+	  for(var i = 0; i<oscArray.length ; i++)
+	  {
+      if(buttons[i][0].className == 'matrixButtOn')
+		  {oscArray[i].connect(audioCtx.destination);}
+	  }    
+  }
+  else
+  {
+    this.className = 'matrixButtOff';
+	  for(var i = 0; i<oscArray.length ; i++)
+	  {
+      if(buttons[i][0].className == 'matrixButtOn')
+		  {oscArray[i].disconnect(audioCtx.destination);}
+	  }
+  }
+}
