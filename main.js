0a2e913b91a2ed0619906b1c135d1982f33f2a56
GridAdmin <gridadmin@inazuma.org>
2024-03-06 21:04:28 -0300

    making 12 tone scale work

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ onOff.style.right =0;
 onOff.className = "matrixButtOff";
 onOff.addEventListener('click',unmute, false);
 document.getElementById("matrix").appendChild(onOff);   
-let virgins = new Array;       
+let previousState = new Array;     
+let virgins = new Array;     
+let virgin = true;       
+
 
 
 let AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -25,23 +28,29 @@ scrubber.className = 'scrubber';
 document.getElementById("matrix").appendChild(scrubber); 
 
 let currentIteration = 0;
+let previousIteration = 15; 
 
-setInterval(eachTick, 500);
-
-for(var i = 0; i<16 ; i++)
-{
-  let currentOsc = audioCtx.createOscillator();
-  currentOsc.frequency.setValueAtTime (Math.pow(octave, (offsets[i]) / subdivisions) * rootNote ,audioCtx.currentTime)   //rootNote*numClicked;
-  currentOsc.startedAlready = false;
-  oscArray.push(currentOsc);//audioCtx.createOscillator());
-  virgins.push(true);
-  //let osc = audioCtx.createOscillator();
-}
+setInterval(eachTick, 250);
 
 let buttons = new Array;
 createGrid(buttons);
 
-buttons[4][5].className = 'matrixButtOn';
+for(var i = 0; i<16 ; i++)
+{
+  //buttons[i][i].className = 'matrixButtOn';
+  let currentOsc = audioCtx.createOscillator();
+  currentOsc.frequency.setValueAtTime (Math.pow(octave, (offsets[i]) / subdivisions) * rootNote ,audioCtx.currentTime)   //rootNote*numClicked;
+  currentOsc.startedAlready = false;
+  oscArray.push(currentOsc);//audioCtx.createOscillator());
+  previousState.push(false);
+  virgins.push(true);
+  //let osc = audioCtx.createOscillator();
+}
+
+//buttons[1][15].className = 'matrixButtOn';
+
+
+
 
 function unmute(event)
 {
@@ -53,28 +62,70 @@ function unmute(event)
   // oscArray[numClicked].frequency.setValueAtTime ( rootNote*numClicked,audioCtx.currentTime);
   // console.log("freq after: " + oscArray[numClicked].frequency );
  
-  if(virgins[numClicked] ===true)
+  if(virgin == true)
   {
+    oscArray.forEach(osc => 
+    {
+      // console.log("index : " + oscArray.indexOf(osc));
+      // console.log("osc");
+      // console.log(osc);
+      osc.start();
+      //osc.connect(audioCtx.destination);
 
-    oscArray[numClicked].start();
-    virgins[numClicked] = false;
+      // console.log("osc");
+      // console.log(osc);
+    });
+    virgin = false;
   }
-  if(this.className == 'matrixButtOff')
-  {
-    this.className = 'matrixButtOn';
-    oscArray[numClicked].connect(audioCtx.destination);
+  // if(virgins[numClicked] ===true)
+  // {
+
+  //   oscArray[numClicked].start();
+  //   virgins[numClicked] = false;
+  // }
+  // if(this.className == 'matrixButtOff')
+  // {
+  //   this.className = 'matrixButtOn';
+  //   oscArray[numClicked].connect(audioCtx.destination);
     
-  }
-  else
-  {
-    this.className = 'matrixButtOff';
+  // }
+  // else
+  // {
+  //   this.className = 'matrixButtOff';
     
-    oscArray[numClicked].disconnect(audioCtx.destination);
-    numClicked = numClicked <15 ? numClicked +1 : 0;
+  //   oscArray[numClicked].disconnect(audioCtx.destination);
+  //   numClicked = numClicked <15 ? numClicked +1 : 0;
 
+  // }
+
+
+}
+
+function unmuteSelectedNotes()
+{
+  if (virgin == true)
+  {
+    console.log("forever virgin");
+    return;
   }
+  buttons[currentIteration].forEach(button => 
+  {
 
+    let currentIndex =   buttons[currentIteration].indexOf(button);
+    //let previousIndex =  buttons[currentIteration].indexOf(button) == 0 ? 15 : buttons[currentIteration].indexOf(button)-1;
 
+    if(button.className != 'matrixButtOff' && (buttons[previousIteration][currentIndex].className == 'matrixButtOff' || virgins[currentIndex] == true  )){
+      console.log("hit turn on");
+      oscArray[currentIndex].connect(audioCtx.destination);
+    }
+    else if (button.className == 'matrixButtOff' && (buttons[previousIteration][currentIndex].className == 'matrixButtOn'  || virgins[currentIndex] == false  ))
+    {
+      console.log("hit turn off");
+
+      oscArray[currentIndex].disconnect(audioCtx.destination);
+    }
+
+  });
 }
 
 
@@ -82,14 +133,17 @@ function eachTick()
 {
 	if(parseInt(scrubber.style.left.substring(0,scrubber.style.left.length-2)) < 600)
 	{
+    currentIteration++;
+    updatePreviousIteration();
 		this.incrementBar();
 	}
 	else
 	{
+    currentIteration = 0;
+    updatePreviousIteration();
 		this.resetBar();
 	}
-
-	
+  unmuteSelectedNotes();
 }
 
 function incrementBar()
@@ -97,8 +151,19 @@ function incrementBar()
 	let tem = scrubber.style.left.substring(0,scrubber.style.left.length-2);
 		tem = parseInt(tem)+37.9;
 		tem += "px";
-		console.log("TEM "  + tem);
+		//console.log("TEM "  + tem);
 		scrubber.style.left = tem;
+}
+
+function updatePreviousIteration()
+{
+  if(currentIteration ==0)
+  {
+    previousIteration = 15;
+  }
+  else{
+    previousIteration = currentIteration-1;
+  }
 }
 
 function resetBar()
